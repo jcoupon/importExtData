34fff6e07ad88b6c4cc06543a859b9c4f03d7d28
Jean Coupon <jean.coupon@gmail.com>
2015-06-16 20:41:42 +0200

    update on EmulateHscCoaddTask

--- python/hsc/importExtData/emulateHscCoadd.SAVED.py
+++ python/hsc/importExtData/emulateHscCoadd.SAVED.py
@@ -21,189 +21,179 @@
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
 
-import numpy
+import pyfits as fits
+
 
 import lsst.pex.config as pexConfig
-import lsst.afw.geom as afwGeom
 import lsst.afw.image as afwImage
-import lsst.coadd.utils as coaddUtils
-import lsst.pipe.base as pipeBase
-from lsst.meas.algorithms import CoaddPsf
-from lsst.pipe.tasks.coaddBase import CoaddBaseTask
-from lsst.pipe.tasks.warpAndPsfMatch import WarpAndPsfMatchTask
-from lsst.pipe.tasks.coaddHelpers import groupPatchExposures, getGroupDataRef
+from   lsst.pipe.tasks.makeCoaddTempExp import MakeCoaddTempExpTask
+
+from lsst.pipe.tasks.calibrate import CalibrateTask
+
+import lsst.daf.base as dafBase
+import lsst.afw.table as afwTable
+import lsst.meas.algorithms as measAlg
 
 
-__all__ = ["EmulateHscCoaddTask"]
-
-class EmulateHscCoaddConfig(CoaddBaseTask.ConfigClass):
-    """Config for EmulateHscCoaddTask
-    """
-    warpAndPsfMatch = pexConfig.ConfigurableField(
-        target = WarpAndPsfMatchTask,
-        doc = "Task to warp and PSF-match calexp",
-    )
-    doWrite = pexConfig.Field(
-        doc = "persist <coaddName>Coadd_tempExp",
-        dtype = bool,
-        default = True,
-    )
-    doOverwrite = pexConfig.Field(
-        doc = "overwrite <coaddName>Coadd_tempExp; If False, continue if the file exists on disk",
-        dtype = bool,
-        default = True,
-    )
-    bgSubtracted = pexConfig.Field(
-        doc = "Work with a background subtracted calexp?",
-        dtype = bool,
-        default = False,
-    )
-
-
-class EmulateHscCoaddTask(CoaddBaseTask):
-    """Task to produce <coaddName>Coadd_tempExp images
-    """
-    ConfigClass = EmulateHscCoaddConfig
-    _DefaultName = "makeCoaddTempExp"
+class EmulateHscCoaddConfig(MakeCoaddTempExpTask.ConfigClass):
+    fileOutName = pexConfig.Field("Name of output file", str, "exposure.fits")
     
-    def __init__(self, *args, **kwargs):
-        CoaddBaseTask.__init__(self, *args, **kwargs)
-        self.makeSubtask("warpAndPsfMatch")
+    calibrate = pexConfig.ConfigurableField(
+        target = CalibrateTask,
+        doc = "Calibration (inc. high-threshold detection and measurement)",
+    )
 
-    @pipeBase.timeMethod
+    detection    = pexConfig.ConfigurableField(
+        target = measAlg.SourceDetectionTask,
+        doc = "Initial (high-threshold) detection phase for calibration",
+    )
+    initialMeasurement = pexConfig.ConfigurableField(
+        target = measAlg.SourceMeasurementTask,
+        doc = "Initial measurements used to feed PSF determination and aperture correction determination",
+    )
+ 
+    #    filterPolicy = pexPolicy.Policy()
+    #    filterPolicy.add("lambdaEff", 470.0)
+    #    afwImage.Filter.define(afwImage.FilterProperty("g", filterPolicy))
+    # see http://hsca.ipmu.jp/doxygen/3.6.1/page_p_a_f.html
+    
+class EmulateHscCoaddTask(MakeCoaddTempExpTask):
+
+
+    #_DefaultName = "EmulateHscCoadd"
+    ConfigClass  = EmulateHscCoaddConfig
+
+    def __init__(self, **kwargs):
+        
+        MakeCoaddTempExpTask.__init__(self, **kwargs)
+        self.makeSubtask("calibrate")
+
+
+        self.schema = afwTable.SourceTable.makeMinimalSchema()
+        self.algMetadata = dafBase.PropertyList()
+        
+        #self.schema = self.calibrate.schema
+        #self.algMetadata = self.calibrate.algMetadata
+
+        self.makeSubtask("detection", schema=self.schema)
+        self.makeSubtask("initialMeasurement", schema=self.schema, algMetadata=self.algMetadata)
+
+
     def run(self, patchRef, selectDataList=[]):
-        """Produce <coaddName>Coadd_tempExp images
-        
-        <coaddName>Coadd_tempExp are produced by PSF-matching (optional) and warping.
-        
-        @param[in] patchRef: data reference for sky map patch. Must include keys "tract", "patch",
-            plus the camera-specific filter key (e.g. "filter" or "band")
-        @return: dataRefList: a list of data references for the new <coaddName>Coadd_tempExp
 
-        @warning: this task assumes that all exposures in a coaddTempExp have the same filter.
-        
-        @warning: this task sets the Calib of the coaddTempExp to the Calib of the first calexp
-        with any good pixels in the patch. For a mosaic camera the resulting Calib should be ignored
-        (assembleCoadd should determine zeropoint scaling without referring to it).
-        """
-        skyInfo = self.getSkyInfo(patchRef)
 
-        calExpRefList = self.selectExposures(patchRef, skyInfo, selectDataList=selectDataList)
+        exposure = patchRef.get('raw')
+
+        idFactory = afwTable.IdFactory.makeSimple()
+        calib = self.calibrate.run(exposure, idFactory=idFactory, expId=1)
+          
 
 
 
-        if len(calExpRefList) == 0:
-            self.log.warn("No exposures to coadd for patch %s" % patchRef.dataId)
-            return None
-        self.log.info("Selected %d calexps for patch %s" % (len(calExpRefList), patchRef.dataId))
-    
-        calExpRefList = [calExpRef for calExpRef in calExpRefList if calExpRef.datasetExists("calexp")]
+        exit(-1)
+
+
+
+
+
+
+
+
+
+
+
+        # test
+        coadd    = patchRef.get(self.config.coaddName + "Coadd")
+
+
+
+
+        if False:
+            coadd.getMaskedImage().getImage().writeFits(   '/Users/coupon/data/tmp/test_img.fits')
+            coadd.getMaskedImage().getVariance().writeFits('/Users/coupon/data/tmp/test_var.fits')
+            coadd.getMaskedImage().getMask().writeFits(    '/Users/coupon/data/tmp/test_msk.fits')
+      
+        skyInfo  = self.getSkyInfo(patchRef)
+        exposure = afwImage.ExposureF(skyInfo.bbox, skyInfo.wcs)
+
+        # to do: check if input wcs and reference coadd have same wcs
+        maskedImage = afwImage.MaskedImageF(
+            afwImage.ImageF("/Users/coupon/data/tmp/test_img.fits"), 
+            afwImage.MaskU( "/Users/coupon/data/tmp/test_msk.fits"), 
+            afwImage.ImageF("/Users/coupon/data/tmp/test_var.fits"))
+        exposure.setMaskedImage(maskedImage)
+
+        #exposure.writeFits(self.config.fileOutName)
+
+
+        exposure.setFilter(coadd.getFilter())
+
        
+        self.calibrate.installInitialPsf(exposure)
+
+        idFactory = afwTable.IdFactory.makeSimple()
      
-        self.log.info("Processing %d existing calexps for patch %s" % (len(calExpRefList), patchRef.dataId))
-
-        tempExpName = self.getTempExpDatasetName()
-
-
-
-
-        groupData = groupPatchExposures(patchRef, calExpRefList, self.getCoaddDatasetName(), tempExpName)
-        self.log.info("Processing %d tempExps for patch %s" % (len(groupData.groups), patchRef.dataId))
-
-        dataRefList = []
-        for i, (tempExpTuple, calexpRefList) in enumerate(groupData.groups.iteritems()):
-            tempExpRef = getGroupDataRef(patchRef.getButler(), tempExpName, tempExpTuple, groupData.keys)
+        table = afwTable.SourceTable.make(self.schema, idFactory)
+        table.setMetadata(self.algMetadata)
+        detRet = self.detection.makeSourceCatalog(table, exposure)
+        sources = detRet.sources
 
 
 
-            if not self.config.doOverwrite and tempExpRef.datasetExists(datasetType=tempExpName):
-                self.log.info("tempCoaddExp %s exists; skipping" % (tempExpRef.dataId,))
-                dataRefList.append(tempExpRef)
-                continue
 
-            self.log.info("Processing tempExp %d/%d: id=%s" % (i, len(groupData.groups), tempExpRef.dataId))
+     
+        self.initialMeasurement.measure(exposure, sources)
 
-            # TODO: mappers should define a way to go from the "grouping keys" to a numeric ID (#2776).
-            # For now, we try to get a long integer "visit" key, and if we can't, we just use the index
-            # of the visit in the list.
-            try:
-                visitId = long(tempExpRef.dataId["visit"])
-            except (KeyError, ValueError):
-                visitId = i
+        matches = None
 
-            exp = self.createTempExp(calexpRefList, skyInfo, visitId)
-            if exp is not None:
-                dataRefList.append(tempExpRef)
-                if self.config.doWrite:
-                    self.writeCoaddOutput(tempExpRef, exp, "tempExp")
-            else:
-                self.log.warn("tempExp %s could not be created" % (tempExpRef.dataId,))
-        return dataRefList
 
-    def createTempExp(self, calexpRefList, skyInfo, visitId=0):
-        """Create a tempExp from inputs
+        for s in sources:
+            print s.getIxx(), s.getIyy()
+     
 
-        We iterate over the multiple calexps in a single exposure to construct
-        the warp ("tempExp") of that exposure to the supplied tract/patch.
 
-        Pixels that receive no pixels are set to NAN; this is not correct
-        (violates LSST algorithms group policy), but will be fixed up by
-        interpolating after the coaddition.
 
-        @param calexpRefList: List of data references for calexps that (may)
-            overlap the patch of interest
-        @param skyInfo: Struct from CoaddBaseTask.getSkyInfo() with geometric
-            information about the patch
-        @param visitId: integer identifier for visit, for the table that will
-            produce the CoaddPsf
-        @return warped exposure, or None if no pixels overlap
-        """
-        inputRecorder = self.inputRecorder.makeCoaddTempExpRecorder(visitId)
-        coaddTempExp = afwImage.ExposureF(skyInfo.bbox, skyInfo.wcs)
-        coaddTempExp.getMaskedImage().set(numpy.nan, afwImage.MaskU.getPlaneBitMask("NO_DATA"), numpy.inf)
-        totGoodPix = 0
-        didSetMetadata = False
-        modelPsf = self.config.modelPsf.apply(skyInfo.wcs) if self.config.doPsfMatch else None
-        for calExpInd, calExpRef in enumerate(calexpRefList):
-            self.log.info("Processing calexp %d of %d for this tempExp: id=%s" %
-                          (calExpInd+1, len(calexpRefList), calExpRef.dataId))
-            try:
-                ccdId = calExpRef.get("ccdExposureId", immediate=True)
-            except Exception:
-                ccdId = calExpInd
-            numGoodPix = 0
-            try:
-                # We augment the dataRef here with the tract, which is harmless for loading things
-                # like calexps that don't need the tract, and necessary for meas_mosaic outputs,
-                # which do.
-                calExpRef = calExpRef.butlerSubset.butler.dataRef("wcs", dataId=calExpRef.dataId,
-                                                                  tract=skyInfo.tractInfo.getId())
-                calExp = self.getCalExp(calExpRef, bgSubtracted=self.config.bgSubtracted)
-                exposure = self.warpAndPsfMatch.run(calExp, modelPsf=modelPsf, wcs=skyInfo.wcs,
-                                                    maxBBox=skyInfo.bbox).exposure
-                if didSetMetadata:
-                    mimg = exposure.getMaskedImage()
-                    mimg *= (coaddTempExp.getCalib().getFluxMag0()[0] / exposure.getCalib().getFluxMag0()[0])
-                    del mimg
-                numGoodPix = coaddUtils.copyGoodPixels(
-                    coaddTempExp.getMaskedImage(), exposure.getMaskedImage(), self.getBadPixelMask())
-                totGoodPix += numGoodPix
-                self.log.logdebug("Calexp %s has %d good pixels in this patch (%.1f%%)" %
-                                  (calExpRef.dataId, numGoodPix, 100.0*numGoodPix/skyInfo.bbox.getArea()))
-                if numGoodPix > 0 and not didSetMetadata:
-                    coaddTempExp.setCalib(exposure.getCalib())
-                    coaddTempExp.setFilter(exposure.getFilter())
-                    didSetMetadata = True
-            except Exception, e:
-                self.log.warn("Error processing calexp %s; skipping it: %s" % (calExpRef.dataId, e))
-                continue
-            inputRecorder.addCalExp(calExp, ccdId, numGoodPix)
 
-        inputRecorder.finish(coaddTempExp, totGoodPix)
-        if totGoodPix > 0 and didSetMetadata:
-            coaddTempExp.setPsf(modelPsf if self.config.doPsfMatch else
-                                CoaddPsf(inputRecorder.coaddInputs.ccds, skyInfo.wcs))
+        psfRet = self.calibrate.measurePsf.run(exposure, sources, expId=0, matches=matches)
+        
+        cellSet = psfRet.cellSet
+        psf = psfRet.psf
 
-        self.log.info("coaddTempExp has %d good pixels (%.1f%%)" %
-                      (totGoodPix, 100.0*totGoodPix/skyInfo.bbox.getArea()))
-        return coaddTempExp if totGoodPix > 0 and didSetMetadata else None
+
+        return result
+
+
+
+
+
+       
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
