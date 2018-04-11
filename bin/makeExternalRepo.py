157ca00352ea0ec8a6dc4a79e916c5091cddcb81
Jim Bosch <jbosch@astro.princeton.edu>
2018-04-11 15:05:34 +0200

    Add dataset definitions and a script to add them.

---
+++ bin/makeExternalRepo.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+from lsst.daf.persistence import RepositoryCfg, Policy, Butler, PosixStorage
+
+
+def main(parent, root):
+    # Construct a Butler just to make a root directory with a
+    # repositoryCfg.yaml file.
+    Butler(inputs=[parent], outputs=[root])
+    # Load that YAML file, and create a copy that adds the extra policy
+    # items from this package.
+    oldConfig = PosixStorage.getRepositoryCfg(root)
+    newPolicy = Policy(Policy.defaultPolicyFile("importExtData",
+                                                "importExtData.yaml",
+                                                "policy"))
+    newConfig = RepositoryCfg(root=oldConfig.root, mapper=oldConfig.mapper,
+                              mapperArgs=oldConfig.mapperArgs,
+                              parents=oldConfig.parents,
+                              policy=newPolicy)
+    # Remove the old YAML file.
+    os.remove(os.path.join(root, "repositoryCfg.yaml"))
+    # Write the new one in its place.
+    PosixStorage.putRepositoryCfg(newConfig)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(
+        description=("Create an HSC data repository with extra dataset "
+                     "definitions for imported external datasets.")
+    )
+    parser.add_argument("parent", metavar="PARENT", type=str,
+                        help=("Directory containing the parent repository to "
+                              "use for inputs (a regular HSC data "
+                              "repository).  May be a rerun."))
+    parser.add_argument("root", metavar="ROOT", type=str,
+                        help=("Directory that will contain the new repository "
+                              "with the new dataset definitions."))
+    parser.parse_args()
+    main(parent=parser.parent, root=parser.root)
