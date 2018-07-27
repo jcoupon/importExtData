bb7eea45268cb69b2cdbc97ff462ae39f2b6a444
Jean Coupon <jean.coupon@gmail.com>
2018-07-27 14:33:12 +0200

    add new config for detection sources

--- config/detectCoaddSources_fixedThreshold.py
+++ config/detectCoaddSources_fixedThreshold.py
@@ -1,5 +1,9 @@
-# from lsst.meas.algorithms import SourceDetectionTask
-# config.detection.retarget(SourceDetectionTask)
+from lsst.meas.algorithms import SourceDetectionTask
+config.detection.retarget(SourceDetectionTask)
 
-config.detection.thresholdValue=7.0
-config.detection.thresholdType='stdev'
+config.detection.thresholdValue = 6.5
+config.doScaleVariance = True
+config.detection.thresholdType = 'pixel_stdev'
+
+# config.detection.thresholdValue=7.0
+# config.detection.thresholdType='stdev'
